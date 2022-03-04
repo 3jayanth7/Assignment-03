d7987c2f2c697e73c3bbc476cb123237888a6b01
3jayanth7 <99715245+3jayanth7@users.noreply.github.com>
2022-03-04 14:47:43 +0530

    Create AssignmentPoJO.java

---
+++ src/POJOassignment/AssignmentPoJO.java
@@ -0,0 +1,51 @@
+package POJOassignment;
+
+public class AssignmentPoJO {
+	// POJO = plain old  Java Object
+	
+		
+//		instance variable (declared as private)
+		private String username;
+		private String password;
+		private String firstName;
+		private String lastName;
+		
+	//  getters and setters for the above instance variables
+//	    go to source and we can find getters and setters
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+
+
+	}
+
+
